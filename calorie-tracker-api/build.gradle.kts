152b5c6aeeffb05f7867c75589926d58643b6f0a
nf <nf@pop-os.localdomain>
2022-05-21 23:57:39 +0200

    configured gradle with kotlin dsl

---
+++ calorie-tracker-api/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	id("org.springframework.boot") version "2.6.7"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	`java-library`
+}
+
+group = "nfrank1995.de"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = org.gradle.api.JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+// object DependencyVersions {
+// 	const val TESTCONTAINERS_VERSION = "1.16.2"
+// }
+
+extra.set("testcontainersVersion", "1.16.2")
+
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:mongodb")
+	testImplementation("org.testcontainers:postgresql")
+}
+
+dependencyManagement {
+	imports {
+		// mavenBom("org.testcontainers:testcontainers-bom:${DependencyVersions.TESTCONTAINERS_VERSION}")
+		mavenBom("org.testcontainers:testcontainers-bom:${rootProject.extra.get("kotlinVersion")}")
+	}
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
